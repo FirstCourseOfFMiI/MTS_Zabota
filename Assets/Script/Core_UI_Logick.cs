fe5fcc172d6ac67a258a9bfb8be9bd84335cecaa
Vlad Moskalchuk <mmvvrr1@gmail.com>
2019-05-25 11:31:14 +0900

    Распаковка

---
+++ Assets/Script/Core_UI_Logick.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core_UI_Logick : MonoBehaviour {
+
+    public GameObject Menu;
+    public GameObject Home;
+    public GameObject Track;
+    public GameObject Nyny;
+    public GameObject Buy;
+
+    public void F_Home()
+    {
+        Home.SetActive(true);
+        Track.SetActive(false);
+        Buy.SetActive(false);
+        Nyny.SetActive(false);
+        Menu.SetActive(false);
+    }
+
+    public void F_Track()
+    {
+        Home.SetActive(false);
+        Track.SetActive(true);
+        Buy.SetActive(false);
+        Nyny.SetActive(false);
+        Menu.SetActive(false);
+
+    }
+
+    public void F_Buy()
+    {
+        Home.SetActive(false);
+        Track.SetActive(false);
+        Buy.SetActive(true);
+        Nyny.SetActive(false);
+        Menu.SetActive(false);
+
+    }
+
+    public void F_Nyny()
+    {
+        Home.SetActive(false);
+        Track.SetActive(false);
+        Buy.SetActive(false);
+        Nyny.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void F_Menu()
+    {
+        Home.SetActive(false);
+        Track.SetActive(false);
+        Buy.SetActive(false);
+        Nyny.SetActive(false);
+        Menu.SetActive(true);
+    }
+
+}
---
+++ Assets/Script/UI_Logick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Logick : MonoBehaviour {
+
+    public GameObject StartPanel;
+    public GameObject _Registr;
+    public GameObject _Registr_Dopl;
+    public GameObject _Vhod;
+
+	public void Registeracia()
+    {
+        StartPanel.SetActive(false);
+        _Registr.SetActive(true);
+    }
+
+    public void Vhod()
+    {
+        StartPanel.SetActive(false);
+        _Vhod.SetActive(true);
+    }
+
+    public void Back()
+    {
+        StartPanel.SetActive(true);
+        _Registr.SetActive(false);
+        _Vhod.SetActive(false);
+    }
+
+    public void Registeracia_Dopl()
+    {
+        _Registr_Dopl.SetActive(true);
+        _Registr.SetActive(false);
+    }
+
+    public void Back_D()
+    {
+        _Registr_Dopl.SetActive(false);
+        _Registr.SetActive(true);
+    }
+
+}
